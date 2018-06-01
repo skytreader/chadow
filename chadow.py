333db804419994e79c4b63b5cb13ba5af1af9038
Chad Estioco <chadestioco@gmail.com>
2018-06-01 22:52:41 +0800

    Add living spec readme and write createlib for chadow script.

--- chadow.py
+++ chadow.py
@@ -1,13 +1,45 @@
 import click
+import os
+import json
+
+VERSION = "0.1.0"
+APP_ROOT = "~/.chadow"
+CONFIG_NAME = "config.json"
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-def createlib():
-    # print("Creating data library %s..." % name)
-    print("Hello world.")
+@click.argument("name")
+def createlib(name):
+    def __createlib(cfg_file):
+        config = json.load(cfg_file)
+
+        version = config.get("version")
+        if version and version != VERSION:
+            print("WARNING: loading a chadow config from an old version.")
+        elif not version:
+            print("WARNING: config does not specify a version.")
+
+        existing_libraries = config.get("libraries", {})
+
+        if name in existing_libraries:
+            print("ERROR: specified name is already taken. Delete name first if you really want to use this name.")
+        else:
+            existing_libraries[name] = {
+                "sectors": {}
+            }
+
+        config["libraries"] = existing_libraries
+
+    try:
+        with open(os.path.join(APP_ROOT, CONFIG_NAME)) as config_file:
+            __createlib(config_lib)
+    except FileNotFoundError:
+        # Fresh chadow install
+        with open(os.path.join(APP_ROOT, CONFIG_NAME), "w+") as config_file:
+            __createlib(config_file)
 
 if __name__ == "__main__":
     cli()
--- readme.markdown
+++ readme.markdown
@@ -1,1 +1,15 @@
 # chadow
+
+Scenario: A very large library of data (like, say, a big bunch of photos) is
+backed-up redundantly across _n_ storage sectors. Each storage sector in turn
+maybe comprised of _m_ different storage media. Now, you want to ensure that
+the file back-ups are consistent and complete across your storage sectors.
+
+Explicitly built for Linux (Ubuntu) for the meantime.
+
+## Installation and Quickstart
+
+Install via the included `install` script. This requires virtualenv goodies as
+installed via [virtualenv-burrito](https://github.com/brainsik/virtualenv-burrito).
+
+Create a new data library to track with the `createlib` command.
