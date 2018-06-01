81eae6d541d4122cfde23cdb348a36048419914a
Chad Estioco <chadestioco@gmail.com>
2018-06-01 23:06:48 +0800

    Define deletelib command.

--- chadow.py
+++ chadow.py
@@ -10,22 +10,27 @@ CONFIG_NAME = "config.json"
 def cli():
     pass
 
+def __version_check(cfg_dict):
+    """
+    Side-effect-ful version check.
+    """
+    version = cfg_dict.get("version")
+    if version and version != VERSION
+        print("WARNING: loading a chadow config from an old version.")
+    elif not version:
+        print("WARNING: config does not specify a version.")
+
 @cli.command()
 @click.argument("name")
 def createlib(name):
     def __createlib(cfg_file):
         config = json.load(cfg_file)
-
-        version = config.get("version")
-        if version and version != VERSION:
-            print("WARNING: loading a chadow config from an old version.")
-        elif not version:
-            print("WARNING: config does not specify a version.")
-
+        __version_check(cfg_dict)
         existing_libraries = config.get("libraries", {})
 
         if name in existing_libraries:
             print("ERROR: specified name is already taken. Delete name first if you really want to use this name.")
+            exit(1)
         else:
             existing_libraries[name] = {
                 "sectors": {}
@@ -37,9 +42,27 @@ def createlib(name):
         with open(os.path.join(APP_ROOT, CONFIG_NAME)) as config_file:
             __createlib(config_lib)
     except FileNotFoundError:
-        # Fresh chadow install
+        # Maybe a botched install. But let's be forgiving anyway.
         with open(os.path.join(APP_ROOT, CONFIG_NAME), "w+") as config_file:
             __createlib(config_file)
+
+@cli.command()
+@click.argument("name")
+def deletelib(name):
+    try:
+        with open(os.path.join(APP_ROOT, CONFIG_NAME)) as config_file:
+            config = json.load(cfg_file)
+            __version_check(cfg_dict)
+            existing_libraries = config.get("libraries", {})
+
+            if name in existing_libraries:
+                del existing_libraries[name]
+            else:
+                print("ERROR: asked to delete a nonexistent library.")
+                exit(1)
+    except FileNotFoundError:
+        print("ERROR: config file not found. Is chadow installed properly?")
+        exit(1)
 
 if __name__ == "__main__":
     cli()
