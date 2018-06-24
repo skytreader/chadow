ebb4c069bedabd531f2d2990a2bd308f5f7c1a4a
Chad Estioco <chadestioco@gmail.com>
2018-06-24 21:01:32 +0800

    Start distinguishing between error types.

--- chadow.py
+++ chadow.py
@@ -16,6 +16,12 @@ APP_ROOT = os.path.expanduser("~/.chadow")
 CONFIG_NAME = "config.json"
 CHADOW_METADATA = ".chadow-metadata"
 
+# Exit codes
+# FIXME Some exit instances probably don't conform at all to these conventions.
+CONFIG_NOT_FOUND = 1
+METADATA_NOT_FOUND = 2
+STATE_CONFLICT = 3
+
 @click.group()
 def cli():
     pass
@@ -47,7 +53,7 @@ def createlib(name):
 
         if name in existing_libraries:
             logging.error("specified name is already taken. Delete name first if you really want to use this name.")
-            exit(1)
+            exit(STATE_CONFLICT)
         else:
             existing_libraries[name] = {
                 "sectors": {}
@@ -91,10 +97,10 @@ def deletelib(name):
                 __write_cfg(config, config_filename, "Deleted library: %s" % name)
             else:
                 logging.error("asked to delete a nonexistent library.")
-                exit(1)
+                exit(STATE_CONLICT)
     except FileNotFoundError:
         logging.error("config file not found. Is chadow installed properly?")
-        exit(1)
+        exit(CONFIG_NOT_FOUND)
 
 @cli.command()
 @click.argument("library")
@@ -107,35 +113,32 @@ def regsector(library, sector_name, sector_path):
         config_filename = os.path.join(APP_ROOT, CONFIG_NAME)
         with open(os.path.join(APP_ROOT, CONFIG_NAME)) as config_file:
             config = json.load(config_file)
-            logging.info("Found config")
             __version_check(config)
 
             libsectors = config["libraries"][library]["sectors"]
             if sector_name in libsectors:
                 logging.error("sector %s already exists in library %s" % (sector_name, library))
-                exit(1)
+                exit(STATE_CONFLICT)
             
         metadata_path = os.path.join(sector_path, CHADOW_METADATA)
         if os.path.isfile(metadata_path):
             logging.error("specified sector_path %s is already registered." % sector_path)
 
-        logging.info("writing metadata...")
-        with open(os.path.join(sector_path, CHADOW_METADATA), "w+") as metadata:
-            metadata.write(sector_name)
+        try:
+            with open(os.path.join(sector_path, CHADOW_METADATA), "w+") as metadata:
+                metadata.write(sector_name)
+        except FileNotFoundError:
+            logging.error("metadata file not found. Did you specify the correct sector path?")
+            exit(METADATA_NOT_FOUND)
         
         config["libraries"][library]["sectors"][sector_name] = sector_path
-        logging.info("Updating cfg...")
         __write_cfg(
             config, config_filename,
             "Created sector %s for library %s at %s." % (sector_name, library, sector_path)
         )
     except FileNotFoundError:
-        # Metadata file is opened as "w+" so we can be confident that this is
-        # from opening the config file.
-        import traceback
-        traceback.print_exc()
         logging.error("config file not found. Is chadow installed properly?")
-        exit(1)
+        exit(CONFIG_NOT_FOUND)
 
 if __name__ == "__main__":
     cli()
