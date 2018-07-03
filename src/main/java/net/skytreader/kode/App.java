54f58ddf3bf8a3edd9dd410a5d1a311ab1675062
Chad Estioco <chadestioco@gmail.com>
2018-07-03 14:20:02 +0800

    Finally be able to run (and fix) this.

--- src/main/java/net/skytreader/kode/App.java
+++ src/main/java/net/skytreader/kode/App.java
@@ -13,9 +13,9 @@ public class App
     public static void main( String[] args )
     {
         Stack<File> directories = new Stack<File>();
-        directories.push(new File("/home/chad/Documents");
+        directories.push(new File("/home/chad/Documents"));
 
-        while (directories.size()){
+        while (directories.size() > 0){
             File curDir = directories.pop();
             System.out.println(curDir.getAbsoluteFile());
             String[] curDirContents = curDir.list();
