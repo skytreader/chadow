0e32a7a7c98c230b0c13ab4c38a4fa5f582df763
Chad Estioco <chadestioco@gmail.com>
2018-07-08 07:35:39 +0800

    Some more boring code.

---
+++ src/main/java/net/skytreader/chadow/ChadowConsistencyChecker.java
@@ -0,0 +1,6 @@
+package net.skytreader.chadow;
+
+interface ChadowConsistencyChecker{
+
+    public void indexSector(String library, String sectorName, String sectorPath); 
+}
--- src/main/java/net/skytreader/chadow/models/Library.java
+++ src/main/java/net/skytreader/chadow/models/Library.java
@@ -1,4 +1,37 @@
 package net.skytreader.chadow.models;
 
 public class Library{
+    private String name;
+    private String comparator;
+    private Sector[] sectors;
+
+    public Library(String name, String comparator, Sector[] sectors){
+        this.name = name;
+        this.comparator = comparator;
+        this.sectors = sectors;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getComparator(){
+        return comparator;
+    }
+
+    public Sector[] getSectors(){
+        return sectors;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setComparator(String comparator){
+        this.comparator = comparator;
+    }
+
+    public void setSectors(Sector[] sectors){
+        this.sectors = sectors;
+    }
 }
