0ffe8abdd6a7786849b914e49bdc0697eab4a0f8
Chad Estioco <chadestioco@gmail.com>
2022-04-06 05:35:05 +0800

    savepoint one more assert

--- chadow_test.py
+++ chadow_test.py
@@ -17,12 +17,14 @@ class ChadowTests(unittest.TestCase):
 
     def setUp(self):
         self.runner = CliRunner()
+        self.full_config_path = os.path.join(chadow.APP_ROOT, chadow.CONFIG_NAME)
 
     def test_createlib(self):
         mo = unittest.mock.mock_open(read_data= DEFAULT_CONFIG_MOCK_VALUE)
         with unittest.mock.patch("chadow.open", mo) as mopen:
             self.runner.invoke(chadow.createlib, ["testlib"])
-            mopen.assert_any_call(os.path.join(chadow.APP_ROOT, chadow.CONFIG_NAME), "r")
+            mopen.assert_any_call(self.full_config_path, "r")
+            mopen.assert_any_call(self.full_config_path, "w")
 
 if __name__ == "__main__":
     tests = unittest.TestLoader().loadTestsFromTestCase(ChadowTests)
