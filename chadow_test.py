ebfaf482c4ea4b8a0c13f72da1c73a8717333125
Chad Estioco <chadestioco@gmail.com>
2022-12-02 16:12:28 +0100

    Reorganize tests.

--- chadow_test.py
+++ chadow_test.py
@@ -23,17 +23,19 @@ class ChadowTests(unittest.TestCase):
         self.runner = CliRunner()
         self.full_config_path = os.path.join(chadow.APP_ROOT, chadow.CONFIG_NAME)
 
-    def __verify_call(self, click_fn, args, expected_return=0):
+    def _verify_call(self, click_fn, args, expected_return=0):
         result = self.runner.invoke(click_fn, args)
 
         if result.exception is not None:
             traceback.print_exception(*result.exc_info)
         self.assertEqual(result.exit_code, expected_return)
 
+class CreateLibTests(ChadowTests):
+
     def test_createlib(self):
         mo = unittest.mock.mock_open(read_data=DEFAULT_CONFIG_MOCK_VALUE)
         with unittest.mock.patch("chadow.open", mo) as mopen, unittest.mock.patch("chadow.os.mkdir") as mmkdir:
-            self.__verify_call(chadow.createlib, ["testlib"])
+            self._verify_call(chadow.createlib, ["testlib"])
             mopen.assert_any_call(self.full_config_path, "r")
             mopen.assert_any_call(self.full_config_path, "w")
             mmkdir.assert_any_call(os.path.join(chadow.APP_ROOT, "testlib"))
@@ -41,13 +43,13 @@ class ChadowTests(unittest.TestCase):
     def test_createlib_corrupted_config(self):
         mo = unittest.mock.mock_open(read_data="{")
         with unittest.mock.patch("chadow.open", mo) as mopen:
-            self.__verify_call(chadow.createlib, ["testlib"], ExitCodes.INVALID_CONFIG.value)
+            self._verify_call(chadow.createlib, ["testlib"], ExitCodes.INVALID_CONFIG.value)
             mopen.assert_any_call(self.full_config_path, "r")
 
     @unittest.mock.patch("chadow.os.mkdir")
     @unittest.mock.patch("chadow.open", new_callable=unittest.mock.mock_open, read_data=DEFAULT_CONFIG_MOCK_VALUE)
     def test_createlib_corrupted_config_force_recreate(self, open_mock, mkdir_mock):
-        self.__verify_call(chadow.createlib, ["testlib", "--force"])
+        self._verify_call(chadow.createlib, ["testlib", "--force"])
         open_mock.assert_any_call(self.full_config_path, "r")
         open_mock.assert_any_call(self.full_config_path, "w")
         mkdir_mock.assert_any_call(os.path.join(chadow.APP_ROOT, "testlib"))
@@ -58,9 +60,11 @@ class ChadowTests(unittest.TestCase):
         config["libraryMapping"]["testlib"] = chadow.make_default_lib("filename")
         mo = unittest.mock.mock_open(read_data=json.dumps(config))
         with unittest.mock.patch("chadow.open", mo) as mopen:
-            self.__verify_call(chadow.createlib, ["testlib"], ExitCodes.STATE_CONFLICT.value)
+            self._verify_call(chadow.createlib, ["testlib"], ExitCodes.STATE_CONFLICT.value)
             mopen.assert_any_call(self.full_config_path, "r")
             mkdir_mock.assert_not_called()
+
+class DeleteLibTests(ChadowTests):
 
     @unittest.mock.patch("chadow.os.rmdir")
     @unittest.mock.patch("chadow.json.dump")
@@ -69,11 +73,35 @@ class ChadowTests(unittest.TestCase):
         config["libraryMapping"]["testlib"] = chadow.make_default_lib("filename")
         mo = unittest.mock.mock_open(read_data=json.dumps(config))
         with unittest.mock.patch("chadow.open", mo) as mopen:
-            self.__verify_call(chadow.deletelib, ["testlib"])
+            self._verify_call(chadow.deletelib, ["testlib"])
+            mock_json_dump.assert_called_with(DEFAULT_CONFIG, unittest.mock.ANY)
+            mopen.assert_any_call(self.full_config_path, "rw")
+            mock_rmdir.assert_called_once_with(os.path.join(chadow.APP_ROOT, "testlib"))
+
+    @unittest.mock.patch("chadow.os.rmdir")
+    @unittest.mock.patch("chadow.json.dump")
+    @unittest.mock.patch("chadow.open", new_callable=unittest.mock.mock_open, read_data=DEFAULT_CONFIG_MOCK_VALUE)
+    def test_deletelib_nonexistent_lib(self, open_mock, mock_json_dump, mock_rmdir):
+        self._verify_call(chadow.deletelib, ["testlib"], chadow.ExitCodes.STATE_CONFLICT.value)
+        mock_json_dump.assert_not_called()
+        open_mock.assert_any_call(self.full_config_path, "rw")
+        mock_rmdir.assert_called_once_with(os.path.join(chadow.APP_ROOT, "testlib"))
+
+    @unittest.mock.patch("chadow.os.rmdir")
+    @unittest.mock.patch("chadow.json.dump")
+    def test_deletelib_nonexistent_directory(self, mock_json_dump, mock_rmdir):
+        # This is technically not necessary but we still want to ensure that
+        # rmdir is called.
+        mock_rmdir.side_effect = FileNotFoundError()
+        config = copy.deepcopy(DEFAULT_CONFIG)
+        config["libraryMapping"]["testlib"] = chadow.make_default_lib("filename")
+        mo = unittest.mock.mock_open(read_data=json.dumps(config))
+        with unittest.mock.patch("chadow.open", mo) as mopen:
+            self._verify_call(chadow.deletelib, ["testlib"])
             mock_json_dump.assert_called_with(DEFAULT_CONFIG, unittest.mock.ANY)
             mopen.assert_any_call(self.full_config_path, "rw")
             mock_rmdir.assert_called_once_with(os.path.join(chadow.APP_ROOT, "testlib"))
 
 if __name__ == "__main__":
-    tests = unittest.TestLoader().loadTestsFromTestCase(ChadowTests)
+    tests = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
     unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(tests)
